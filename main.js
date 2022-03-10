b616a1891defd38d316b0d9c3648e94317015640
8devil3 <8dev.il3@gmail.com>
2022-03-10 15:55:32 +0100

    generazione di una nuova immagine casuale

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const vueApp = new Vue({
     el: "#root",
     data: {
         msg: "Hello World",
-        img: 'https://picsum.photos/200/300?random=1',
+        img: 'https://picsum.photos/400/250?random=1',
+        baseImgURL: 'https://picsum.photos/',
+        width: 400,
+        height: 250
     },
+    methods: {
+        newImg: function() {
+            this.img = this.baseImgURL + this.width + '/' + this.height + '?random=' + Math.floor(Math.random() * 100);
+        }
+    }
 });
 
